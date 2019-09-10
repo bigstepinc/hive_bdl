ebc381904789924b000ada0db1db917e32c2f5aa
Mateescu Serban-Corneliu <serban.mateescu@gmail.com>
2019-09-10 16:25:42 +0300

    possible fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -139,12 +139,12 @@ cp /opt/bigstepdatalake-$BDLCL_VERSION/lib/hadoop* $HADOOP_HOME/share/hadoop/com
 mkdir /tmp/hive 
 chmod -R 777 /tmp/hive
 
-bdl -mkdir /spark-warehouse
-
 #fix core-site.xml
 cp /core-site.xml /opt/bigstepdatalake-$BDLCL_VERSION/conf/core-site.xml
 cp /core-site.xml $HADOOP_HOME/etc/hadoop/core-site.xml
 cp /core-site.xml $HADOOP_HOME/share/hadoop/common/templates/core-site.xml
+
+bdl -mkdir /spark-warehouse
 
 # fix aws dependencies
 #This is commented for this build
